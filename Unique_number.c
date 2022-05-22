c213f424ba41aedfc729d1d3aaed982c8a50f4f2
Ch-Harshitha <96976684+Ch-Harshitha@users.noreply.github.com>
2022-05-22 20:25:13 +0530

    #include<stdio.h> int main() {     int n,x,r,count=0,i=0,j,ans=0;     scanf("%d",&n);     x=n;     while(n>0)     {         n=n/10;         count++;     }     int arr[count];     while(x>0)     {         r=x%10;         arr[i]=r;         x=x/10;         i++;     }     for(i=0;i<count;i++)     {         for(j=0;j<count;j++)         {             if(arr[i]==arr[j] && i!=j)             {                 printf("Not Unique Number");                 ans++;                 break;             }         }         if(ans>0)         {             break;         } } if(ans==0) {     printf("Unique Number"); } }

---
+++ Unique_number.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+    int n,x,r,count=0,i=0,j,ans=0;
+    scanf("%d",&n);
+    x=n;
+    while(n>0)
+    {
+        n=n/10;
+        count++;
+    }
+    int arr[count];
+    while(x>0)
+    {
+        r=x%10;
+        arr[i]=r;
+        x=x/10;
+        i++;
+    }
+    for(i=0;i<count;i++)
+    {
+        for(j=0;j<count;j++)
+        {
+            if(arr[i]==arr[j] && i!=j)
+            {
+                printf("Not Unique Number");
+                ans++;
+                break;
+            }
+        }
+        if(ans>0)
+        {
+            break;
+        }
+}        
+if(ans==0)
+{
+    printf("Unique Number");
+}
+}
