060dc73a5a9fc7a5eb7b3fcab94b8d0cc8f248c1
Ch-Harshitha <96976684+Ch-Harshitha@users.noreply.github.com>
2022-06-29 23:21:14 +0530

    #include<stdio.h> int main() {     int l,n,i=0;     int w,h;     scanf("%d",&l);     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d%d",&w,&h);         if(w<l || h<l)         printf("UPLOAD ANOTHER ");         else if(w==h)         printf("ACCEPTED ");         else if(w==l && h==l)         printf("ACCEPTED ");         else if(w>l || h>l)         printf("CROP IT ");     } }

---
+++ Roy_and_Profile_Picture.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int l,n,i=0;
+    int w,h;
+    scanf("%d",&l);
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d%d",&w,&h);
+        if(w<l || h<l)
+        printf("UPLOAD ANOTHER
+");
+        else if(w==h)
+        printf("ACCEPTED
+");
+        else if(w==l && h==l)
+        printf("ACCEPTED
+");
+        else if(w>l || h>l)
+        printf("CROP IT
+");
+    }
+}
