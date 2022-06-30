41ef79e29ef5b4c1fd38017b6456e34e7a0cd0d2
Ch-Harshitha <96976684+Ch-Harshitha@users.noreply.github.com>
2022-06-30 23:34:24 +0530

    #include<stdio.h> int main() {     int n,max;     scanf("%d",&n);     int a[n],i;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     int p,q,s=0;     scanf("%d %d",&p,&q);     for(i=0;i<n;i++)     {         if(a[i]>=p && a[i]<=q)         {         s=s+a[i];     } } printf("%d",s); }

---
+++ sum_of_elements_between_A_and_B.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n,max;
+    scanf("%d",&n);
+    int a[n],i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int p,q,s=0;
+    scanf("%d %d",&p,&q);
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>=p && a[i]<=q)
+        {
+        s=s+a[i];
+    }
+}
+printf("%d",s);
+}
+    
