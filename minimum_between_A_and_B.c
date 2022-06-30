00427222f599c32cf3ea40ce961445996a554ecf
Ch-Harshitha <96976684+Ch-Harshitha@users.noreply.github.com>
2022-06-30 23:27:12 +0530

    #include<stdio.h> #include<limits.h> int main() {     int n,max=INT_MAX;     scanf("%d",&n);     int a[n],i;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     int p,q;     scanf("%d %d",&p,&q);     for(i=0;i<n;i++)     {         if(a[i]>=p && a[i]<=q)         {             if(max>a[i])             {                 max=a[i];             }         }     }     if(max==INT_MAX)     {         printf("-1");     }     else     {         printf("%d",max);     } }

---
+++ minimum_between_A_and_B.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<limits.h>
+int main()
+{
+    int n,max=INT_MAX;
+    scanf("%d",&n);
+    int a[n],i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int p,q;
+    scanf("%d %d",&p,&q);
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>=p && a[i]<=q)
+        {
+            if(max>a[i])
+            {
+                max=a[i];
+            }
+        }
+    }
+    if(max==INT_MAX)
+    {
+        printf("-1");
+    }
+    else 
+    {
+        printf("%d",max);
+    }
+}
